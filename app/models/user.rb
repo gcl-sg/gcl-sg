becc3406c44d196014efd252a9116bfde3ac5ce8
kenniz <kennizchan@gmail.com>
2016-07-24 18:42:13 +0800

    add user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable, :recoverable,
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  private
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      conditions[:username].downcase! if conditions[:username]
+      where(conditions.to_h).first
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Admin account"
+User.find_or_create_by!(username: 'admin') do |user|
+  user.email = "admin@example.com"
+  user.password = "nandudu"
+  user.password_confirmation = "nandudu"
+end
